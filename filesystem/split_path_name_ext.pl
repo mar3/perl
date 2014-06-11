8f39d435ecfb4f7c61e85f14bb5f17e2187fe97f
mar3 <irisawa.masaru@gmail.com>
2014-06-11 13:54:01 +0900

    none

--- filesystem/split_path_name_ext.pl
+++ filesystem/split_path_name_ext.pl
@@ -1,3 +1,10 @@
+#!/usr/bin/perl
+# coding: utf-8
+
+use strict;
+use File::Spec::Functions;
+
+
 sub _split_path_part {
 
 	my ($path) = @_;
@@ -8,14 +15,58 @@ sub _split_path_part {
 
 	my $dotted = rindex($name, '.');
 	if($dotted == -1) {
-		# 拡張子が無い名前
 		return $parent, $name, '';
 	}
 	elsif($dotted == 0) {
-		# .ではじまり、拡張子が無い(=hidden file)
 		return $parent, $name, '';
 	}
 	else {
-		return $parent, $name, substr($name, $dotted + 1);
+		return $parent, substr($name, 0, $dotted), substr($name, $dotted + 1);
 	}
 }
+
+sub _test {
+
+	my $path = shift;
+	my ($parent, $name, $ext) = _split_path_part($path);
+	printf('path=[%s] >>> parent=[%s], name=[%s], ext=[%s]', $path, $parent, $name, $ext);
+	print("\n");
+}
+
+sub _main {
+
+	_test(undef);
+	_test('');
+
+	_test('.');
+	_test('..');
+	_test('...');
+	_test('....');
+
+	_test('simple name');
+
+	_test('normal.suffix');
+	_test('normal.txt.suffix');
+	_test('a.b.c.d');
+	_test('a....b.....c.....d');
+	_test('x.');
+	_test('x..');
+	_test('x...');
+	_test('xx.');
+	_test('xx..');
+	_test('xx...');
+
+	_test('.hidden file');
+	_test('.hidden file.suffix');
+	_test('..abnormal suffix');
+	_test('..abnormal name and.suffix');
+	_test('...abnormal name and.suffix');
+
+	_test('/root file');
+	_test('/root file.suffix');
+	_test('/.root hidden file');
+	_test('/.root hidden file.suffix');
+	_test('/.root hidden file.middle name.suffix');
+}
+
+_main(@ARGV);
