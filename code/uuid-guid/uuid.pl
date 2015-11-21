6334215141427cf3c7d6c3d69bf875dfda73e29e
mass10 <irisawa.masaru@gmail.com>
2015-11-21 17:12:49 +0900

    wip

--- code/uuid-guid/uuid.pl
+++ code/uuid-guid/uuid.pl
@@ -10,6 +10,9 @@ sub _main {
 
 	my $generator = Data::UUID->new;
 	print($generator->create_str(), "\n");
+
+	print('[www.mycompany.com] という文字列を使用して UUID を生成する。(毎回同じものが生成される)', "\n");
+	print($generator->create_from_name_str(NameSpace_URL, 'www.mycompany.com'), "\n");
 }
 
 _main();
