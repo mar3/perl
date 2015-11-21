e204e4776b6119e70cfa804944ddc60ef08d10a0
mass10 <irisawa.masaru@gmail.com>
2015-11-21 19:26:54 +0900

    wip

---
+++ code/database/elasticsearch/enumerate.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl
+# coding: utf-8
+
+use strict;
+use utf8;
+use YAML;
+use JSON;
+use Search::Elasticsearch;
+
+sub _println {
+
+	print(@_, "\n");
+}
+
+sub _open_connection {
+
+	my $connection = Search::Elasticsearch->new(nodes => ['192.168.141.160:9200']);
+	return $connection;
+}
+
+sub _search_random {
+
+	my $connection = _open_connection();
+	if(!$connection->indices->exists(index => 'sake_database')) {
+		_println('[WARN] "sake_databse" not found.');
+		return;
+	}
+	my $results = $connection->search(index => 'sake_database', body => {});
+	my $hits = $results->{hits};
+
+	# _println(YAML::Dump($hits));
+
+	_println('max_score=', $hits->{max_score});
+	_println('total=', $hits->{total});
+	foreach my $e (@{$hits->{hits}}) {
+		_println(JSON::to_json($e, {pretty => 1}));
+	}
+}
+
+sub _main {
+
+	binmode(STDIN, ':utf8');
+	binmode(STDOUT, ':utf8');
+	binmode(STDERR, ':utf8');
+
+	_search_random();
+}
+
+_main();
