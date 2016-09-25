4a7c1a37b5816d48222f3e74ce22a0b2bdca1462
mass10 <mass10.github@gmail.com>
2016-09-25 02:39:01 +0000

    wip

--- code/yaml/yaml-to-json.pl
+++ code/yaml/yaml-to-json.pl
@@ -5,7 +5,6 @@ use strict;
 use utf8;
 use YAML;
 use JSON;
-use Data::Dumper;
 
 sub _main {
 
@@ -16,22 +15,10 @@ sub _main {
 	my @lines = <STDIN>;
 	my $content = join('', @lines);
 	$content = YAML::Load($content);
-	# print(YAML::Dump($content), "\n");
-
-	if(1) {
-		local $Data::Dumper::Indent = 1;
-		local $Data::Dumper::Sortkeys = 1;
-		local $Data::Dumper::Terse = 1;
-		print(Data::Dumper::Dumper($content), "\n");
-		return;
-	}
-
-	if(1) {
-		$content = JSON::to_json($content, {pretty => 1});
-		print($content);
-		print("\n");
-		return;
-	}
+	$content = JSON::to_json($content, {pretty => 1});
+	print($content);
+	print("\n");
+	return;
 }
 
 _main();
