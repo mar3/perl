74affd1f36457eaa5c4c165367a53a4e189998d0
mass10 <mass10.github@gmail.com>
2016-10-09 11:22:11 +0900

    wip

--- code/networking/smtp/main.pl
+++ code/networking/smtp/main.pl
@@ -5,16 +5,25 @@
 use strict;
 use Net::SMTP;
 
-sub _main {
+sub _send {
 
 	my $s = Net::SMTP->new('127.0.0.1', Timeout => 15);
 	$s->mail('root@example.jp');
-	$s->to('postmaster@example.jp');
+	$s->to('mick.jagger@gmail.com');
 	$s->data();
-	$s->datasend('subject: how are you?');
+	$s->datasend('Date: 2015-12-24 23:58:58', "\n");
+	$s->datasend('Subject: This is test.', "\n");
+	$s->datasend('From: You <robert.plant@gmail.com>', "\n");
+	$s->datasend('Content-Transfer-Encoding: ISO-2022-JP', "\n");
+	$s->datasend('Content-Type: text/plain; charset=ISO-2022-JP', "\n");
 	$s->datasend('body');
 	$s->dataend();
 	$s->quit();
+}
+
+sub _main {
+
+	_send();
 }
 
 _main();
