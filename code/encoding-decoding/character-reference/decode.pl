e4d553db7aaaf68ac7002191f997d104fa6a7489
mass10 <mass10.github@gmail.com>
2017-01-21 07:07:11 +0900

    wip

---
+++ code/encoding-decoding/character-reference/decode.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+# coding: utf-8
+
+use strict;
+use utf8;
+use HTML::Entities;
+
+sub _main {
+
+	binmode(STDIN, ':utf8');
+	binmode(STDOUT, ':utf8');
+	binmode(STDERR, ':utf8');
+
+	foreach my $line (<STDIN>) {
+		print(HTML::Entities::decode_entities($line));
+	}
+}
+
+_main(@ARGV);
