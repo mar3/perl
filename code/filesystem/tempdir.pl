4273cfecdd173c382678bd3a8f85f34f637d2b41
mass10 <mass10.github@gmail.com>
2017-12-23 11:11:15 +0900

    wip

--- code/filesystem/tempdir.pl
+++ code/filesystem/tempdir.pl
@@ -9,8 +9,20 @@
 use strict;
 use File::Temp;
 
+
+sub _test_tmpfile {
+
+	# 一時ファイル
+	my $tmp = File::Temp->new(UNLINK => 1, SUFFIX => '.dat');
+
+	print($tmp "Hello\n");
+	print($tmp "Hello\n");
+	print($tmp "Hello\n");
+}
+
 sub _main {
 
+	# 一時ディレクトリ
 	my $unknown = File::Temp::tempdir();
 
 	print('[', $unknown, ']', "\n");
